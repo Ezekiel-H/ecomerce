1996c28c19f1e50f8756218bbc9b288059eb671e
Ezekiel-H <Ezekiel.j.p.h@gmail.com>
2017-12-29 16:11:32 +0000

    Product Updates

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+	title			= models.CharField(max_length=120)
+	description		= models.TextField(null=True)
+	price			= models.DecimalField(decimal_places=2, max_digits=10, default=39.99)
+
+	def __str__(self):
+		return self.title
+	#this references product rather than product type
